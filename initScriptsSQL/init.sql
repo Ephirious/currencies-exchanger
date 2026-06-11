394d4177ae28e3777a78a3d9cc2f3a5f7dc8aa0f
Danila <Ephirious@gmail.com>
2026-06-11 20:24:01 +0300

    feat: добавил генерацию тестовых данных при инициализации контейнера

--- initScriptsSQL/init.sql
+++ initScriptsSQL/init.sql
@@ -12,5 +12,18 @@ CREATE TABLE exchange_rates (
     rate DECIMAL NOT NULL CHECK(rate = round(rate, 6)),
     CONSTRAINT unique_currency_pair UNIQUE (base_currency_id, target_currency_id),
     CONSTRAINT check_different_currencies CHECK (base_currency_id != target_currency_id)
-)
+);
+
+INSERT INTO currencies (code, fullname, sign)
+VALUES
+    ('USD', 'US Dollar', '$'),
+    ('EUR', 'Euro', '€'),
+    ('RUB', 'Russian Ruble', '₽'),
+    ('GBP', 'British Pound', '£'),
+    ('JPY', 'Japanese Yen', '¥'),
+    ('CNY', 'Chinese Yuan', '¥'),
+    ('KZT', 'Kazakhstani Tenge', '₸'),
+    ('BYN', 'Belarusian Ruble', 'Br'),
+    ('TRY', 'Turkish Lira', '₺'),
+    ('AED', 'UAE Dirham', 'د.إ');
 

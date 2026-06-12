81d68a43c28a2539a612ef01b3be7fd568cc34f7
Danila <Ephirious@gmail.com>
2026-06-12 23:20:19 +0300

    feat: подключено в общий контейнер Jackson-маппер, изменён формат вывода на JSON у /currencies

--- src/main/java/com/ephirious/controller/CurrenciesServlet.java
+++ src/main/java/com/ephirious/controller/CurrenciesServlet.java
@@ -1,43 +1,46 @@
 package com.ephirious.controller;
 
+import com.ephirious.config.ServletsConfig;
 import com.ephirious.container.ApplicationContainer;
 import com.ephirious.dao.CurrencyDao;
 import com.ephirious.dto.CurrencyDTO;
 import com.ephirious.listener.ApplicationContext;
 import com.ephirious.services.CurrencyService;
-import jakarta.servlet.ServletContext;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 @WebServlet("/currencies")
 public class CurrenciesServlet extends HttpServlet {
     private CurrencyService currencyService;
+    private ObjectMapper mapper;
 
     @Override
     public void init() throws ServletException {
         ApplicationContainer container = (ApplicationContainer) getServletContext()
                 .getAttribute(ApplicationContext.APPLICATION_ATTRIBUTE);
+
         currencyService = new CurrencyService(container.get(CurrencyDao.class));
+        mapper = container.get(ObjectMapper.class);
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        final String CONTENT_TYPE = "text/plain; charset=UTF-8";
-        response.setContentType(CONTENT_TYPE);
-        PrintWriter responseWriter = response.getWriter();
-        for (CurrencyDTO currency : currencyService.getCurrencies()) {
-            responseWriter.printf("<p>%s %s %s %s</p>",
-                    currency.getId(),
-                    currency.getCode(),
-                    currency.getFullname(),
-                    currency.getCode()
-            );
+        response.setContentType(ServletsConfig.JSON_CONTENT_TYPE.getSetting());
+        response.setCharacterEncoding(ServletsConfig.ENCODING.getSetting());
+
+        List<CurrencyDTO> currencies = currencyService.getCurrencies();
+
+        try (ServletOutputStream outputStream = response.getOutputStream()) {
+            mapper.writeValue(outputStream, currencies);
         }
     }
 }
--- src/main/java/com/ephirious/listener/ApplicationContext.java
+++ src/main/java/com/ephirious/listener/ApplicationContext.java
@@ -3,6 +3,7 @@ package com.ephirious.listener;
 import com.ephirious.container.ApplicationContainer;
 import com.ephirious.dao.CurrencyDao;
 import com.ephirious.db.ConnectionPool;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
@@ -17,9 +18,11 @@ public class ApplicationContext implements ServletContextListener {
 
         ConnectionPool pool = new ConnectionPool();
         CurrencyDao currencyDao = new CurrencyDao(pool.getDataSource());
+        ObjectMapper mapper = new ObjectMapper();
 
         container.put(ConnectionPool.class, pool);
         container.put(CurrencyDao.class, currencyDao);
+        container.put(ObjectMapper.class, mapper);
 
         event.getServletContext().setAttribute(APPLICATION_ATTRIBUTE, container);
     }
@@ -27,7 +30,6 @@ public class ApplicationContext implements ServletContextListener {
     @Override
     public void contextDestroyed(ServletContextEvent event) {
         ApplicationContainer container = (ApplicationContainer) event.getServletContext().getAttribute(APPLICATION_ATTRIBUTE);
-        ConnectionPool pool = container.get(ConnectionPool.class);
-        pool.close();
+        container.get(ConnectionPool.class).close();
     }
 }

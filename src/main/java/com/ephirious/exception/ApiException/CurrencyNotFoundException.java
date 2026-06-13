506fa12755d7f761c78d05a685484bfa0e192323
Danila <Ephirious@gmail.com>
2026-06-13 16:24:09 +0300

    feat: добавлено исключение для обозначений отсутсвия валюты в БД

--- src/main/java/com/ephirious/exception/ApiException/CurrencyNotFoundException.java
+++ src/main/java/com/ephirious/exception/ApiException/CurrencyNotFoundException.java
@@ -2,7 +2,7 @@ package com.ephirious.exception.ApiException;
 
 public class CurrencyNotFoundException extends BaseApiException {
     private static final String DEFAULT_MESSAGE = "Валюта по коду %s не была найдена";
-    private static final int DEFAULT_STATUS_CODE = 400;
+    private static final int DEFAULT_STATUS_CODE = 404;
 
     public CurrencyNotFoundException(String code) {
         super(DEFAULT_STATUS_CODE, DEFAULT_MESSAGE.formatted(code));

fca272f8f63a145b8cd7a381ac9aa4cc9fe6784e
Danila <Ephirious@gmail.com>
2026-06-13 22:42:25 +0300

    refactor: изменена логика обработки запросов согласно новому общему DAOBase-классу

--- src/main/java/com/ephirious/dao/CurrencyDao.java
+++ src/main/java/com/ephirious/dao/CurrencyDao.java
@@ -1,79 +1,59 @@
 package com.ephirious.dao;
 
 import com.ephirious.entities.Currency;
-import com.ephirious.exception.ApiException.DaoException;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class CurrencyDao {
+public class CurrencyDao extends BaseDAO {
     private static final int ID_INDEX = 1;
     private static final int CODE_INDEX = 2;
     private static final int FULLNAME_INDEX = 3;
     private static final int SIGN_INDEX = 4;
 
     private static final String FIND_ALL = """
-            SELECT *
+            SELECT id, code, fullname, sign
             FROM currencies
             """;
 
     private static final String FIND_BY_CODE = """
-            SELECT *
+            SELECT id, code, fullname, sign
             FROM currencies
             WHERE code = ?
             """;
 
-    private final DataSource dataSource;
-
     public CurrencyDao(DataSource dataSource) {
-        this.dataSource = dataSource;
+        super(dataSource);
     }
 
     public List<Currency> findAll() {
-        List<Currency> currencies = new ArrayList<>();
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(FIND_ALL);
-             ResultSet result = statement.executeQuery()) {
-            while (result.next()) {
-                currencies.add(new Currency(
-                        result.getLong(ID_INDEX),
-                        result.getString(CODE_INDEX),
-                        result.getString(FULLNAME_INDEX),
-                        result.getString(SIGN_INDEX))
-                );
-            }
-        } catch (SQLException exception) {
-            throw new DaoException("Ошибка при получении списка валют");
-        }
-
-        return currencies;
+        return queryList(
+                FIND_ALL,
+                this::mapCurrency,
+                "Ошибка при получении списка валют"
+        );
     }
 
     public Optional<Currency> findByCode(String code) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(FIND_BY_CODE)) {
-            statement.setString(1, code);
+        return queryOptional(
+                FIND_BY_CODE,
+                (statement) -> {
+                    statement.setString(1, code);
+                },
+                this::mapCurrency,
+                "Ошибка при получении валюты по коду"
+        );
+    }
 
-            try (ResultSet result = statement.executeQuery()) {
-                if (result.next()) {
-                    return Optional.of(new Currency(
-                            result.getLong(ID_INDEX),
-                            result.getString(CODE_INDEX),
-                            result.getString(FULLNAME_INDEX),
-                            result.getString(SIGN_INDEX)
-                    ));
-                }
-            }
-        } catch (SQLException exception) {
-            throw new DaoException("Ошибка при получении валюты по коду");
-        }
-
-        return Optional.empty();
+    private Currency mapCurrency(ResultSet result) throws SQLException {
+        return new Currency(
+                result.getLong(ID_INDEX),
+                result.getString(CODE_INDEX),
+                result.getString(FULLNAME_INDEX),
+                result.getString(SIGN_INDEX)
+        );
     }
 }

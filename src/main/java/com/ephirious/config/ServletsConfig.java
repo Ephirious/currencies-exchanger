8e1ea8adc60ae0410ff7304e04b2cfcf33327a05
Danila <Ephirious@gmail.com>
2026-06-14 15:15:44 +0300

    fix: убрана ошибка в типе контента

--- src/main/java/com/ephirious/config/ServletsConfig.java
+++ src/main/java/com/ephirious/config/ServletsConfig.java
@@ -3,7 +3,7 @@ package com.ephirious.config;
 public enum ServletsConfig {
     JSON_CONTENT_TYPE("application/json"),
     ENCODING("UTF-8"),
-    X_WWW_FORM_URLENCODED_CONTENT_TYPE("application/x-www-form-unlencoded");
+    X_WWW_FORM_URLENCODED_CONTENT_TYPE("application/x-www-form-urlencoded");
 
     private final String setting;
 

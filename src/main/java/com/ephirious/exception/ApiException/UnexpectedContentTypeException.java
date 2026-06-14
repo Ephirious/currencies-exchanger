ebdc17778f8740d602ad3d1f8f6d721f074d98fb
Danila <Ephirious@gmail.com>
2026-06-14 13:55:39 +0300

    feat: добавлено исключение, указывающие на некорректный тип контента

---
+++ src/main/java/com/ephirious/exception/ApiException/UnexpectedContentTypeException.java
@@ -0,0 +1,16 @@
+package com.ephirious.exception.ApiException;
+
+public class UnexpectedContentTypeException extends BaseApiException {
+    private static final int DEFAULT_STATUS_CODE = 415;
+    private static final String DEFAULT_EXCEPTION_MESSAGE = "Неподдерживаемый тип контетна '%s'. Ожидаемые тип '%s'";
+
+    public UnexpectedContentTypeException(String invalid, String expected) {
+        super(DEFAULT_STATUS_CODE, DEFAULT_EXCEPTION_MESSAGE.formatted(invalid, expected));
+    }
+
+    public UnexpectedContentTypeException(int statusCode, String message) {
+        super(statusCode, message);
+    }
+
+
+}

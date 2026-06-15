bba586a5256bc7c4280583f1559b105d203288b0
Danila <Ephirious@gmail.com>
2026-06-15 20:28:53 +0300

    feat: добавлены зависимости для ExchangeRate

--- src/main/java/com/ephirious/listener/ApplicationContext.java
+++ src/main/java/com/ephirious/listener/ApplicationContext.java
@@ -2,10 +2,12 @@ package com.ephirious.listener;
 
 import com.ephirious.container.ApplicationContainer;
 import com.ephirious.dao.CurrencyDao;
+import com.ephirious.dao.ExchangeRateDao;
 import com.ephirious.db.ConnectionPool;
 import com.ephirious.exception.apiexception.daoexception.DaoException;
 import com.ephirious.interfaces.ExceptionMapper;
 import com.ephirious.services.CurrencyService;
+import com.ephirious.services.ExchangeRateService;
 import com.ephirious.util.SQLExceptionMapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletContextEvent;
@@ -24,14 +26,19 @@ public class ApplicationContext implements ServletContextListener {
 
         ConnectionPool pool = new ConnectionPool();
         ExceptionMapper<SQLException, DaoException> exceptionMapper = new SQLExceptionMapper();
+
         CurrencyDao currencyDao = new CurrencyDao(pool.getDataSource(), exceptionMapper);
+        ExchangeRateDao exchangeRateDao = new ExchangeRateDao(pool.getDataSource(), exceptionMapper);
+
         ObjectMapper mapper = new ObjectMapper();
         CurrencyService currencyService = new CurrencyService(currencyDao);
+        ExchangeRateService exchangeRateService = new ExchangeRateService(currencyService, exchangeRateDao);
 
         container.put(ConnectionPool.class, pool);
         container.put(CurrencyDao.class, currencyDao);
         container.put(ObjectMapper.class, mapper);
         container.put(CurrencyService.class, currencyService);
+        container.put(ExchangeRateService.class, exchangeRateService);
 
         event.getServletContext().setAttribute(APPLICATION_ATTRIBUTE, container);
     }

6a320cb7b2fd7ea37f864f890fcfa87f5804c43d
Danila <Ephirious@gmail.com>
2026-06-16 12:28:03 +0300

    feat: возвращёно перечисление со кодами статуса ответа

---
+++ src/main/java/com/ephirious/config/HttpStatusCode.java
@@ -0,0 +1,19 @@
+package com.ephirious.config;
+
+import lombok.Getter;
+
+@Getter
+public enum HttpStatusCode {
+    OK(200),
+    CREATED(201),
+    BAD_REQUEST(400),
+    NOT_FOUND(404),
+    CONFLICT(409),
+    INTERNAL_SERVET_ERROR(500);
+
+    private final int statusCode;
+
+    HttpStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+}
